921b1e2f5f937eff05a003a0d5346b70cd407761
khai20801 <khaingzinwin20801@gmail.com>
2020-10-01 09:00:19 +0630

    test

--- app.js
+++ app.js
@@ -2359,7 +2359,7 @@ const setupPersistentMenu = (res) => {
   };
   // Start the request
   request({
-      url: 'https://graph.facebook.com/v8.0/me/messenger_profile?access_token=' + PAGE_ACCESS_TOKEN,
+      url: 'https://graph.facebook.com/v2.6/me/messenger_profile?access_token=' + PAGE_ACCESS_TOKEN,
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
@@ -2389,7 +2389,7 @@ const removePersistentMenu = (res) => {
     ]
   };
   request({
-      url: 'https://graph.facebook.com/v8.0/me/messenger_profile?access_token=' + PAGE_ACCESS_TOKEN,
+      url: 'https://graph.facebook.com/v2.6/me/messenger_profile?access_token=' + PAGE_ACCESS_TOKEN,
       method: 'DELETE',
       headers: {
         'Content-Type': 'application/json'
